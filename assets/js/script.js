90742bf53a316c19418807036ec2bc3ce3d55f9e
Jorge A Salinas Jr <asalinas212@outlook.com>
2021-12-19 23:46:29 -0600

    added save event handler and block color function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,22 @@
 // save button
-var saveBtnEl = $(".saveBtn");
-// today's date
-var currentDate = $("#todayDate").text(moment().format('dddd MMMM Do YYYY'));
+var saveBtn = $(".saveBtn");
+// update #todayDate with today's date
+$("#todayDate").text(moment().format('dddd MMMM Do YYYY'));
+
+
+// color time blocks
+var blockColors= function() {
+    // get hours
+    var numHours = moment().hours();
+
+
+    console.log(numHours);
+
+};
+
+// save to local storage
+saveBtn.on("click", function () {
+    // store textarea data to localstorage
+});
+
+blockColors();
