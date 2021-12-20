7d4836e448bb7f288ca35abd5e78b470563d327a
Jorge A Salinas Jr <asalinas212@outlook.com>
2021-12-20 05:15:39 -0600

    finished time block color logic

--- README.md
+++ README.md
@@ -9,6 +9,11 @@ A web app that allows you to plan out your workday with hourly time blocks.
 * CSS
 * JavaScript
 
+## Libraries
+* Moment.js
+* jQuery
+* Bootstrap
+
 ## Website
 [Work Day Scheduler Screen Capture](https://jsalinas212.github.io/work-day-scheduler-challenge/)
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -7,11 +7,25 @@ $("#todayDate").text(moment().format('dddd MMMM Do YYYY'));
 // color time blocks
 var blockColors= function() {
     // get hours
-    var numHours = moment().hours();
+    var nowTime = moment().hour();
 
-    // iterate through hours, find present, past, future and add respective color class
+    // iterate through hour blocks, find present, past, future and add respective color class
     $(".time-block").each(function() {
+        var getElId = parseInt($(this).attr("id"));
         // 
+        if (nowTime === getElId) {
+            $(this).removeClass("future");
+            $(this).removeClass("past");
+            $(this).addClass("present");
+        } else if (nowTime < getElId) {
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+            $(this).addClass("past");
+        } else {
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+            $(this).addClass("future");
+        }
     })
 };
 
